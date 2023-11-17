a45cf404dc6cc73eeda1b169e86f382e1f242902
Lumi <shaicloud@gmail.com>
2023-11-17 02:16:58 -0500

    added user greeting to index

--- src/router/router.ts
+++ src/router/router.ts
@@ -28,7 +28,13 @@ router.get("/login", (req: express.Request, res: express.Response) => {
 router.post("/login", userController.login_user_post);
 
 router.get("/index", (req: express.Request, res: express.Response) => {
-	res.render("index");
+	// Check if a user is authenticated
+	const isAuthenticated = req.isAuthenticated();
+
+	// Access user data if authenticated
+	const user = isAuthenticated ? req.user : null;
+
+	res.render("index", { isAuthenticated, user });
 });
 
 router.post("/index", userController.membership_user_post);
